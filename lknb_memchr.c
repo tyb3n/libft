3e04f66e4886f0867daef6212c948d29d6c296e4
m2i <m2i@deb>
2020-02-14 10:37:47 +0100

    Commit lknb./lknb_memchr.c

---
+++ lknb_memchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbenoist <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/24 11:18:34 by tbenoist          #+#    #+#             */
+/*   Updated: 2015/12/11 08:52:16 by tbenoist         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	int				i;
+
+	i = 0;
+	while (i < (int)n)
+	{
+		if (((unsigned char*)s)[i] == (unsigned char)c)
+			return ((void*)(s + i));
+		i++;
+	}
+	return (NULL);
+}
